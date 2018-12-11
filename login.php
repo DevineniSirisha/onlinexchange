8e55c0bed1a97aadad186d2cd6a9db624809ca23
Devineni,Sai Sirisha <S531367@nwmissouri.edu>
2018-12-11 12:39:57 -0600

    commit 4

--- login.php
+++ login.php
@@ -89,12 +89,8 @@
 										<li><a href="login.html">Login</a></li> 
                                     </ul>
                                 </li> 
-								<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
-                                    <ul role="menu" class="sub-menu">
-                                        <li><a href="blog.html">Blog List</a></li>
-										<li><a href="blog-single.html">Blog Single</a></li>
-                                    </ul>
-                                </li> 
+								
+                               
 								<li><a href="Aboutus.html">About Us</a></li> 
 								<li><a href="contact-us.html">Contact</a></li>
 							</ul>
